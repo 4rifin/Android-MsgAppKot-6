7ba3d6eec9ee87f0c91bc96b940653943cee1f02
4rifin <ndms.arifin@gmail.com>
2018-07-11 14:07:29 +0700

    Section two,Video Two

--- app/src/main/java/com/smartherd/msgappkot/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgappkot/MainActivity.kt
@@ -2,11 +2,20 @@ package com.smartherd.msgappkot
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener(){
+            Log.i("MainActivity","Button Was Clicked !!")
+            Toast.makeText(this,"Button Was Clicked !",Toast.LENGTH_SHORT).show()
+        }
     }
 }
