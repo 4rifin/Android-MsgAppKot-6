cd2d184b2521be784f650bf4ea164d89111e5cfe
4rifin <ndms.arifin@gmail.com>
2018-07-12 15:52:02 +0700

    Recycle View - List View And Card View

---
+++ app/src/main/java/com/smartherd/msgappkot/Model.kt
@@ -0,0 +1,30 @@
+package com.smartherd.msgappkot
+
+import android.support.v7.widget.DialogTitle
+
+data class Hobby(var title: String)
+
+object Suplier{
+    val hobbies = listOf<Hobby>(
+            Hobby("Swimming"),
+            Hobby("Reading"),
+            Hobby("Walking"),
+            Hobby("Sleeping"),
+            Hobby("Gaming"),
+            Hobby("Swimming"),
+            Hobby("Reading"),
+            Hobby("Walking"),
+            Hobby("Sleeping"),
+            Hobby("Gaming"),
+            Hobby("Swimming"),
+            Hobby("Reading"),
+            Hobby("Walking"),
+            Hobby("Sleeping"),
+            Hobby("Gaming1"),
+            Hobby("Swimming"),
+            Hobby("Reading"),
+            Hobby("Walking"),
+            Hobby("Sleeping"),
+            Hobby("Gaming")
+    )
+}
